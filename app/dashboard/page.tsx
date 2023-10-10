5ae85a77b8b5ca8449b702b4f3578908698fa0cd
Karan <karanrajmehta06@gmail.com>
2023-10-11 02:38:28 +0530

    Feature: Display Bands on Dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,60 @@
-import UserNav from '@/components/user-nav';
+import { MoreHorizontal } from 'lucide-react';
 
-export default function Page() {
+import { db } from '@/lib/db';
+import { Button } from '@/components/ui/button';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import AddButton from '@/components/add-button';
+import UserNav from '@/components/user-nav';
+import Wrapper from '@/components/wrapper';
+
+export default async function Page() {
+  const bands = await db.band.findMany();
+
   return (
     <>
       <UserNav />
+      <Wrapper>
+        <AddButton />
+        <Table>
+          <TableCaption>A list of available bands.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Serial Number</TableHead>
+              <TableHead>Frequency Range (From)</TableHead>
+              <TableHead>Frequency Range (To)</TableHead>
+              <TableHead>Channel Spacing</TableHead>
+              <TableHead className='text-right'>Generate Frequencies</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {bands.map((band, idx) => (
+              <TableRow key={band.id}>
+                <TableCell className='font-medium'>{idx + 1}</TableCell>
+                <TableCell>{band.from} Hz</TableCell>
+                <TableCell>{band.to} Hz</TableCell>
+                <TableCell>{band.spacing} Hz</TableCell>
+                <TableCell className='text-right'>
+                  <Button
+                    variant='ghost'
+                    size='icon'
+                    className='hover:bg-neutral-200'
+                  >
+                    <MoreHorizontal className='h-4 w-4 stroke-gray-700' />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Wrapper>
     </>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import '@/styles/globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
+import { Toaster } from 'sonner';
 
 import { cn } from '@/lib/utils';
 
@@ -28,6 +29,7 @@ export default function RootLayout({
           )}
         >
           {children}
+          <Toaster closeButton richColors />
         </body>
       </html>
     </ClerkProvider>
