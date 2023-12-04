ecfd44c3ddcdf91bec52e89abc9c2c079478cc50
Karan <karanrajmehta06@gmail.com>
2023-12-05 02:33:22 +0530

    chore: migrate to Next.js 14

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,11 @@
 import '@/styles/globals.css';
 
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
+import { GeistSans } from 'geist/font/sans';
 import { Toaster } from 'sonner';
 
 import { cn } from '@/lib/utils';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://spectrum-control.vercel.app'),
@@ -39,7 +37,7 @@ export default function RootLayout({
         <body
           className={cn(
             'min-h-screen bg-[url("/bg-paper.png")] font-sans antialiased',
-            inter.className
+            GeistSans.className
           )}
         >
           {children}
